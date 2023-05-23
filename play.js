e868034a3b002517b3c9f67b07d62abe7ab53f1e
WillWam <willwamfromscratch@gmail.com>
2023-05-22 22:45:30 -0600

    JS functions to add active players to active players list

--- play.js
+++ play.js
@@ -5,13 +5,31 @@ function DebugA(logMessage) {
     }
 }
 
+let activePlayers = [
+    "Will_Wam",
+    "Cameron",
+    "Test_User",
+    "Awesome_Guy"
+];
+
 function logIn(username) {
     DebugA("logIn");
     const loggedInText = document.getElementById("logged-in-element");
     loggedInText.innerHTML = "Logged in - " + username;
 }
 
+function addActivePlayer(username) {
+    DebugA("addActivePlayer: " + username);
+    const activeUsersUl = document.getElementById("active-players-ul");
+    const newUserItem = document.createElement("SECTION");
+    newUserItem.innerHTML = username;
+    newUserItem.setAttribute("class", "player-item");
+    activeUsersUl.appendChild(newUserItem);
+}
 
+function addAllActivePlayers() {
+    DebugA("addAllActivePlayers");
+    activePlayers.forEach((player) => addActivePlayer(player));
+}
 
-
-
+addAllActivePlayers();
