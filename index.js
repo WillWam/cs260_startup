ae7ed637a38b38edbb06e5597606c4d4841ed038
WillWam <willwamfromscratch@gmail.com>
2023-06-08 12:13:45 -0600

    Added in simple endpoints for login

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
-const db = require('./database.js');
+const DB = require('./database.js');
+
+const authCookieName = 'token';
+
 // import fetch from "node-fetch";
 
 // import { Configuration, OpenAIApi } from "openai";
@@ -15,8 +21,12 @@ const db = require('./database.js');
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
@@ -30,9 +40,70 @@ app.listen(port, () => {
 });
 
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+let secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+
 // Get Scores
 apiRouter.get('/scores', (_req, res) => {
-  scores = db.getLeaderboard();
+  scores = DB.getLeaderboard();
   res.send(scores);
 });
 
@@ -67,7 +138,7 @@ apiRouter.post('/signUp', async (req, res) => {
     medium: 0,
     hard: 0
   }
-  await db.addUser(usernameItem); 
+  await DB.addUser(usernameItem); 
   res.send(usernameItem);
 });
 
@@ -153,7 +224,7 @@ async function askQuestion(questionItem) {
       endDate: new Date(Date.now()),
       questions: questionsLog
     }
-    await db.addFinishedWord(finishedWordItem);
+    await DB.addFinishedWord(finishedWordItem);
     setNewWord();
     return answerItem;
   }
